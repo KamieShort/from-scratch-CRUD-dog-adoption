4ae0dcb03f4f63812aaf3a027561212c69953ccd
Kamie Short <kamie.short@gmail.com>
2022-03-30 12:01:32 -0700

    added auth page, user.js, and routes in App.js, auth page displaying.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './views/Home/Home';
 import DogsDetails from './views/dogsDetails/dogsDetails';
 import DogForm from './views/newForm/newForm';
 import EditPage from './views/EditPage/EditPage';
 import Header from './services/components/Header/Header';
+import Auth from './views/Auth/Auth';
+import { getUser } from './services/users';
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
   return (
     <BrowserRouter>
       <Header />
@@ -14,6 +18,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/auth">
+            <Auth setCurrentUser={setCurrentUser} />
           </Route>
           <Route exact path="/dogs/new">
             <DogForm />
---
+++ src/views/Auth/Auth.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { signInUser } from '../../services/users';
+
+export default function Auth({ setCurrentUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    const resp = await signInUser(email, password);
+    setCurrentUser(resp.email);
+  };
+  return (
+    <div>
+      <h2>Sign In</h2>
+
+      <form onSubmit={submit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+      </form>
+    </div>
+  );
+}
